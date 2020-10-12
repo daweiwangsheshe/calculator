e2d8285c3e27eebaea5ffc67c3423af6bacc1ec2
daweiwangsheshe <72077257+daweiwangsheshe@users.noreply.github.com>
2020-10-12 22:31:40 +0800

    Add files via upload

---
+++ 计算器/CalculationMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TestCalculator
+{
+    class CalculationMethod
+    {
+        private static int result = 0;
+
+        public int Add(int number1, int number2)
+        {
+            result = number1 + number2;
+            return result;
+        }
+        public int Subtraction(int number1, int number2)
+        {
+            result = number1 - number2;
+            return result;
+        }
+        public int Multiplication(int number1, int number2)
+        {
+            result = number1 * number2;
+            return result;
+        }
+        public int Division(int number1, int number2)
+        {
+            result = number1 / number2;
+            return result;
+        }
+    }
+}
